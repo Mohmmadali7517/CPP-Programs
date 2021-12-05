8d230027f7685c5e758642e08ead6e0e5f522472
Mohmmadali7517 <95292726+Mohmmadali7517@users.noreply.github.com>
2021-12-05 12:11:15 +0530

    Add files via upload

---
+++ Program2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int iNo1=0,iNo2=0,iRet=0;
+	cout<<"Enter first Number"<<"\n";
+	cin>>iNo1;
+	cout<<"Enter Second Number"<<"\n";
+	cin>>iNo2;
+	
+	iRet=iNo1+iNo2;
+	cout<<"Addition is :"<<iRet<<"\n";
+	
+	return 0;
+}
+
+
